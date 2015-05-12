f5ee4b37bb31fdce9ebbbdeb6db93eb13d8395de
thiiozz <vermerschth@gmail.com>
2015-05-12 16:37:40 +0200

    fix smarty errors

--- src/main/loader/smartySingleton.php
+++ src/main/loader/smartySingleton.php
@@ -9,6 +9,8 @@ class SmartySingleton{
 	public static function getInstance(){
 		if(is_null(self::$_instance)){
 			self::$_instance = new Smarty();
+            self::$_instance->setTemplateDir(ABS_PATH . "/templates");
+            self::$_instance->setCompileDir(ABS_PATH . "/templates_c");
 			self::$_instance->debugging = SMARTY_DEBUG;
 			self::$_instance->caching = SMARTY_CACHE;
 			self::$_instance->cache_lifetime = SMARTY_CACHE_LIFE;
